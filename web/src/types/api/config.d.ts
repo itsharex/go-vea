55124d0bd33c2304a7bdabc85a614a42baf431f8
aguoxing <2933575030@qq.com>
2022-11-19 16:44:57 +0800

    chore: types

---
+++ web/src/types/api/config.d.ts
@@ -0,0 +1,8 @@
+export type ConfigFormData = {
+  configId: number | undefined
+  configName: string
+  configKey: string
+  configValue: string
+  remark: string
+  configType: string
+}
---
+++ web/src/types/api/notice.d.ts
@@ -0,0 +1,7 @@
+export type NoticeFormData = {
+  noticeId: number | undefined
+  noticeTitle: string
+  noticeType: string
+  noticeContent: string
+  status: string
+}
